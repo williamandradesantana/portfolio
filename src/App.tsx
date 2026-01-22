51c73a1242a113ad564bb47bcff1a3e0541827eb
William Santana <williamandrade1058@gmail.com>
2026-01-22 13:09:13 -0300

    feat: add main component and change background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
 export function App() {
   return (
-    <div className="min-h-screen overflow-y-auto bg-white">
+    <div className="min-h-screen overflow-y-auto bg-neutral-400">
       <Header />
+      <Main />
     </div>
   );
 }
