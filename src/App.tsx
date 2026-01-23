74046d6d2da447b42a1df716097f7af8fde5f906
William Santana <williamandrade1058@gmail.com>
2026-01-22 23:07:55 -0300

    feat(footer): add Footer component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 export function App() {
   return (
-    <div className="min-h-screen overflow-y-auto bg-neutral-400">
+    <div className="min-h-screen flex flex-col overflow-y-auto bg-black">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
