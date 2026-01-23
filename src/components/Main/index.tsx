8b2588e4e46f6c6ad176092fc3c09e56d783bd27
William Santana <williamandrade1058@gmail.com>
2026-01-23 12:09:54 -0300

    feat: add component CertificationsAndEducation to Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,17 @@
 import { About } from "../About";
+import { CertificationsAndEducation } from "../CertificationsAndEducation";
 import { Experiences } from "../Experiences";
 import { Hero } from "../Hero";
 import { Projects } from "../Projects";
 
 export function Main() {
   return (
-    <main className="flex-1 flex flex-col items-center justify-center px-6">
+    <main className="flex-1 flex flex-col items-center mx-auto justify-center px-6">
       <Hero />
       <About />
       <Experiences />
       <Projects />
+      <CertificationsAndEducation />
     </main>
   );
 }
