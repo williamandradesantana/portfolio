f5b1d6b0c9b90804c97369d9c375cce76023166b
William Santana <williamandrade1058@gmail.com>
2026-01-23 12:08:27 -0300

    feat(CertificationsAndEducation): create component CertificationsAndEducation

---
+++ src/components/CertificationsAndEducation/index.tsx
@@ -0,0 +1,107 @@
+import { GraduationCap, Star } from "lucide-react";
+import { SectionList } from "../SectionList";
+import { useEffect, useState } from "react";
+import type { EducationModel } from "../../models/education/education";
+import type { CertificationsModel } from "../../models/certifications/certifications";
+import { InfoCard } from "../InfoCard";
+
+export function CertificationsAndEducation() {
+  const [education, setEducation] = useState<EducationModel>();
+  const [certifications, setCertifications] = useState<CertificationsModel[]>(
+    [],
+  );
+
+  useEffect(() => {
+    async function loadEducation() {
+      const response = await fetch("src/db/seed/education/education.json");
+      const data = await response.json();
+      setEducation(data.education);
+    }
+
+    loadEducation();
+  }, []);
+
+  useEffect(() => {
+    async function loadCertifications() {
+      const response = await fetch(
+        "src/db/seed/certifications/certifications.json",
+      );
+      const data = await response.json();
+      setCertifications(data.certifications);
+    }
+
+    loadCertifications();
+  }, []);
+
+  return (
+    <section
+      id="certifications"
+      className="min-h-screen flex flex-col mt-12 mx-auto px-6 items-center w-full max-w-6xl"
+    >
+      <div className="w-full md:max-w-6xl">
+        <span className="text-zinc-400 text-sm uppercase tracking-widest">
+          Certificações
+        </span>
+        <h2 className="text-white text-3xl font-semibold leading-tight mt-1">
+          Educação e Certificados
+        </h2>
+      </div>
+
+      <div className="w-full mt-12">
+        <h3 className="flex items-center gap-2 text-sm font-medium text-zinc-500 uppercase">
+          <GraduationCap size={16} />
+          Educação
+        </h3>
+
+        <SectionList>
+          {education && (
+            <InfoCard
+              key={education.graduation}
+              title={education.graduation}
+              description={education.description}
+              footer={
+                <div className="flex justify-between text-sm text-zinc-400">
+                  <span>{education.institution}</span>
+                  <span>{education.duration}</span>
+                </div>
+              }
+            />
+          )}
+        </SectionList>
+      </div>
+
+      <div className="w-full mt-12">
+        <h3 className="flex items-center gap-2 text-sm font-medium text-zinc-500 uppercase">
+          <Star size={16} />
+          Certificados
+        </h3>
+
+        <SectionList>
+          {certifications.map((certification, index) => (
+            <InfoCard
+              key={`${certification.name}-${index}`}
+              title={certification.name}
+              description={certification.institution}
+              footer={
+                <div className="flex items-center justify-between text-sm text-zinc-400">
+                  <span>{certification.yearOfCompletion}</span>
+
+                  {certification.link && (
+                    <a
+                      href={certification.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-zinc-300 underline-offset-4 hover:underline hover:text-white transition"
+                    >
+                      Ver certificados
+                    </a>
+                  )}
+                </div>
+              }
+            />
+          ))}
+        </SectionList>
+      </div>
+    </section>
+  );
+}
